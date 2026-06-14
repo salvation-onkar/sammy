004a4ea871d6fab5deeccd8bbb0f534677967c2c
salvation-onkar <xdonkarmsu@gmail.com>
2026-06-14 09:54:28 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girlfriends Day 💓').split('')
+    const titles = ('Happy birthday ^_^').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
